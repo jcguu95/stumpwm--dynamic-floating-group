eb5b66b267f4b085800b6b3a0247044649278b8b
jcguu95 <jcguu95@gmail.com>
2021-04-21 08:19:22 -0500

    Give win+ a proper name!
    (take the todo as well)

--- dynamic-floating-group.lisp
+++ dynamic-floating-group.lisp
@@ -1,12 +1,12 @@
 (load "~/stumpwm--dynamic-floating-group/util.lisp")
 (in-package #:stumpwm)
 
-(defstruct dyn-order :window :free)
-;; (dyn-order-free (make-dyn-order :window 1 :free t))
+(defstruct win+ :window :free)
+;; (win+-free (make-win+ :window 1 :free t))
 
 (defclass dyn-float-group (float-group)
-  ((dyn-order :initform nil
-              :accessor dyn-float-group-dyn-order)))
+  ;; a dyn-order, or a dynamic order, is a list of win+.
+  ((dyn-order :initform nil :accessor dyn-float-group-dyn-order)))
 
 (flet ((add-float-window (group window)
          ;; not sure if needed
@@ -19,8 +19,7 @@
     (add-float-window group window)
     (nconc
      (dyn-float-group-dyn-order group)
-     (list (make-dyn-order :window window
-                           :free nil)))
+     (list (make-win+ :window window :free nil)))
     (re-tile group)))
 
 (defmethod group-delete-window ((group dyn-float-group)
@@ -43,36 +42,34 @@
   ;; If window W is not in the dyn-order, make one for it and
   ;; push into dyn-order.
   (loop for w in (group-windows group)
-        do (unless (member w (mapcar #'dyn-order-window
+        do (unless (member w (mapcar #'win+-window
                                      (dyn-float-group-dyn-order group)))
-             (push (make-dyn-order :window w :free nil)
+             (push (make-win+ :window w :free nil)
                    (dyn-float-group-dyn-order group))))
   ;; If window W+ is not in the list of windows of GROUP, delete
   ;; W+ from the dyn-order.
   (loop for w+ in (dyn-float-group-dyn-order group)
-        do (unless (member (dyn-order-window w+) (group-windows group))
+        do (unless (member (win+-window w+) (group-windows group))
              (deletef (dyn-float-group-dyn-order group) w+)))
   ;; Push free windows to the first elements in the list.
   (setf (dyn-float-group-dyn-order group)
         (concatenate 'list
-                     (remove-if (lambda (dyno) (equal nil (dyn-order-free dyno)))
+                     (remove-if (lambda (dyno) (equal nil (win+-free dyno)))
                                 (dyn-float-group-dyn-order group))
-                     (remove-if (lambda (dyno) (equal t (dyn-order-free dyno)))
+                     (remove-if (lambda (dyno) (equal t (win+-free dyno)))
                                 (dyn-float-group-dyn-order group))))
   ;; Let the (group-windows group) respect the order of
   ;; (dyn-float-group-dyn-order group) FIXME Still need to know
   ;; how to trigger stumpwm to reorder the displayed windows with
   ;; respect to its window list.
   (setf (group-windows group)
-        (mapcar #'dyn-order-window
+        (mapcar #'win+-window
                 (dyn-float-group-dyn-order group))))
 
-;; TODO I should probably explain that window+ is a window and a
-;; its :free state.
 (defun current-window+ (&optional (group (current-group)))
   (let ((gcw (group-current-window group)))
     (find-if (lambda (x)
-               (equal gcw (dyn-order-window x)))
+               (equal gcw (win+-window x)))
              (dyn-float-group-dyn-order group))))
 
 (defun current-window-position (&optional (group (current-group)))
@@ -83,27 +80,27 @@
 (defun free-all (&optional (group (current-group)))
   ;; alias: un-tile-all
   (loop for w+ in (dyn-float-group-dyn-order group)
-        do (setf (dyn-order-free w+) t))
+        do (setf (win+-free w+) t))
   (re-tile group))
 
 (defun unfree-all (&optional (group (current-group)))
   ;; alias: tile-all
   (loop for w+ in (dyn-float-group-dyn-order group)
-        do (setf (dyn-order-free w+) nil))
+        do (setf (win+-free w+) nil))
   (re-tile group))
 
 (defun free-window (&optional (window (current-window))
                       (group (current-group)))
   (loop for w+ in (dyn-float-group-dyn-order group)
-        if (equal window (dyn-order-window w+))
-          do (setf (dyn-order-free w+) t))
+        if (equal window (win+-window w+))
+          do (setf (win+-free w+) t))
   (re-tile group))
 
 (defun unfree-window (&optional (window (current-window))
                         (group (current-group)))
   (loop for w+ in (dyn-float-group-dyn-order group)
-        if (equal window (dyn-order-window w+))
-          do (setf (dyn-order-free w+) nil))
+        if (equal window (win+-window w+))
+          do (setf (win+-free w+) nil))
   (re-tile group))
 
 (defun toggle-freeness-current-window (&optional (window (current-window))
@@ -111,15 +108,15 @@
   ;; TODO make the freed window more explicit.. maybe need to
   ;; make the window smaller or something. also, when it goes
   ;; back it should go to the bottom of the stack.
-  (if (eq (dyn-order-free (current-window+ group)) t)
-      (setf (dyn-order-free (current-window+ group)) nil)
-      (setf (dyn-order-free (current-window+ group)) t))
+  (if (eq (win+-free (current-window+ group)) t)
+      (setf (win+-free (current-window+ group)) nil)
+      (setf (win+-free (current-window+ group)) t))
   (re-tile))
 
 (defun unfloating-windows+ (&optional (group (current-group)))
   "Return the list of window+s whose :FREE slot is nil."
   (remove-if-not
-   (lambda (w+) (eq (dyn-order-free w+) nil))
+   (lambda (w+) (eq (win+-free w+) nil))
    (dyn-float-group-dyn-order group)))
 
 (defun re-tile (&optional (group (current-group)))
@@ -128,7 +125,7 @@
   (let* ((cs (slot-value (current-screen) 'number))
          (sw (xlib:screen-width cs))
          (sh (xlib:screen-height cs))
-         (wl (mapcar #'dyn-order-window (unfloating-windows+ group)))
+         (wl (mapcar #'win+-window (unfloating-windows+ group)))
          (N (length wl)))
     (case N
       (0 nil)
